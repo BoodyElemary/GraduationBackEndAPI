e68544ac36d3ff622f783070b1ad4057ab622884
eman-m0hamed <emanmohamed73359@gmail.com>
2023-06-19 23:10:45 +0300

    edit product model nutrition field

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -28,7 +28,7 @@ const productSchema = new mongoose.Schema(
         required: true,
       },
       nutrition: {
-        type: String,
+        type: mongoose.SchemaTypes.Mixed,
         required: true,
       },
       ingredients: {

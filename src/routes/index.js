34075759f4e388513bf46d3b2a0ed514f9c61f9e
eman-m0hamed <emanmohamed73359@gmail.com>
2023-06-19 17:11:17 +0300

    add toppingType route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,14 @@ const router = express.Router();
 const baseRouter = require(path.join(__dirname, 'base.routes'));
 const flavorRouter = require(path.join(__dirname, 'flavor.routes'));
 const toppingRouter = require(path.join(__dirname, 'topping.routes'));
+const toppingTypeRouter = require(path.join(__dirname, 'toppingType.routes'));
 const categoryRouter = require(path.join(__dirname, 'category.routes'));
 const productRouter = require(path.join(__dirname, 'product.routes'));
 
 //add routes here
 router.use('/api/bases',baseRouter);
 router.use('/api/flavors',flavorRouter);
+router.use('/api/toppingsType',toppingTypeRouter);
 router.use('/api/toppings',toppingRouter);
 router.use('/api/categories',categoryRouter);
 router.use('/api/products',productRouter);

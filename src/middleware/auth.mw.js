c4ec6a45644295cbcb824f696da46d24bda5d311
MahmoudElfakharany <m.a.el-fakharany@hotmail.com>
2023-06-20 18:20:00 +0300

    Clone New Repo Now

--- src/middleware/auth.mw.js
+++ src/middleware/auth.mw.js
@@ -5,7 +5,7 @@ const createError = require(path.join(__dirname, "..", "util", "error"));
 const Admin = mongoose.model("Admin");
 const SuperAdmin = mongoose.model("SuperAdmin");
 
-const isLogin = async (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     let token = req.get("authorization");
     if (!token) next(createError("Login to auth", 403));
@@ -27,27 +27,30 @@ const isLogin = async (req, res, next) => {
   }
 };
 
-const isAuthorized = (req, res, next) => {
-  const allowed = false;
-  let allowedRoutes;
-  if (req.user.role === "customer") {
-    allowedRoutes = [
-      { path: "/api/blabla", methods: ["POST", "GET"] }, //example
-      { path: "/api/blabla", methods: ["GET"] }, //example
-    ];
-  } else if (req.user.role === "admin") {
-    allowedRoutes = [{ path: "/api/admin/profile", methods: ["GET"] }];
-  } else if (req.user.role === "super") {
-    return next();
-  } else next(createError("Unknown user role!!"));
-  allowed = allowedRoutes.some((route) => {
-    const pathMatches = req.path === route.path;
-    const methodMatches = route.methods.includes(req.method);
-    return pathMatches && methodMatches;
-  });
-  if (!allowed) {
-    next(createError("Unauthorized route or method", 401));
-  } else next();
-};
+// const isAuthorized = (req, res, next) => {
+//   let allowed = false;
+//   let allowedRoutes;
+//   if (req.user.role === "customer") {
+//     allowedRoutes = [
+//       { path: "/api/orders/2", methods: ["POST", "GET", "DELETE", "PUT"] },
+//       { path: "/api/orders/3", methods: ["GET", "DELETE", "PUT"] },
+//     ];
+//   } else if (req.user.role === "admin") {
+//     allowedRoutes = [
+//       { path: "/api/1", methods: ["GET", "PUT", "DELETE", "POST"] },
+//       { path: "/api/orders/3", methods: ["GET", "DELETE", "PUT"] },
+//     ];
+//   } else if (req.user.role === "super") {
+//     return next();
+//   } else next(createError("Unknown user role!!"));
+//   allowed = allowedRoutes.some((route) => {
+//     const pathMatches = req.path.includes(route.path);
+//     const methodMatches = route.methods.includes(req.method);
+//     return pathMatches && methodMatches;
+//   });
+//   if (!allowed) {
+//     next(createError("Unauthorized route or method", 401));
+//   } else next();
+// };
 
-module.exports = [isLogin, isAuthorized];
+// module.exports = [isLogin, isAuthorized];

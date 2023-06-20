0756d044f938e280429650b88d27be7df198b974
eman-m0hamed <emanmohamed73359@gmail.com>
2023-06-20 15:27:45 +0300

    edit delete method to be soft and hard delete

--- src/routes/base.routes.js
+++ src/routes/base.routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path');
 const baseController = require(path.join(__dirname, "..", "controller", "base.controller"))
-
 const Router = express.Router({mergeParams:true})
 
 Router.get('/', baseController.index)

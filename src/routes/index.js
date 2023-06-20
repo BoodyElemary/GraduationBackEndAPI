7737e0e13c805a5480457bd32d21e4070983c388
BoodyElemary <abdelrahman_elemary99@outlook.com>
2023-06-20 12:43:20 +0300

    fixing mail link not working and finsishing activision token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const router = express.Router();
 // routes files path
 
-const baseRouter = require(path.join(__dirname, "base.routes"));
-const flavorRouter = require(path.join(__dirname, "flavor.routes"));
-const toppingRouter = require(path.join(__dirname, "topping.routes"));
-const categoryRouter = require(path.join(__dirname, "category.routes"));
-const productRouter = require(path.join(__dirname, "product.routes"));
-const adminRouter = require(path.join(__dirname, "admin.routes"));
-const notFoundRoute = require(path.join(__dirname, "not-found.routes"));
+const baseRouter = require(path.join(__dirname, 'base.routes'));
+const flavorRouter = require(path.join(__dirname, 'flavor.routes'));
+const toppingRouter = require(path.join(__dirname, 'topping.routes'));
+const categoryRouter = require(path.join(__dirname, 'category.routes'));
+const productRouter = require(path.join(__dirname, 'product.routes'));
+const adminRouter = require(path.join(__dirname, 'admin.routes'));
+const notFoundRoute = require(path.join(__dirname, 'not-found.routes'));
 const authenticationRoute = require(path.join(
   __dirname,
-  "authentication.routes"
+  'authentication.routes',
 ));
-
+const customerRoute = require(path.join(__dirname, 'customer.routes'));
 //add routes here
-router.use("/api/login", authenticationRoute);
-router.use("/api/bases", baseRouter);
-router.use("/api/flavors", flavorRouter);
-router.use("/api/toppings", toppingRouter);
-router.use("/api/categories", categoryRouter);
-router.use("/api/products", productRouter);
-router.use("/api/admin", adminRouter);
+router.use('/api/login', authenticationRoute);
+router.use('/api/bases', baseRouter);
+router.use('/api/flavors', flavorRouter);
+router.use('/api/toppings', toppingRouter);
+router.use('/api/categories', categoryRouter);
+router.use('/api/products', productRouter);
+router.use('/api/admin', adminRouter);
+router.use('/api/customers', customerRoute);
 /*
 const baseRouter = require(path.join(__dirname, 'base.routes'));
 const flavorRouter = require(path.join(__dirname, 'flavor.routes'));
@@ -44,6 +45,6 @@ router.use('/api/vouchers',voucherRouter);
 */
 
 // not found route
-router.use("*", notFoundRoute);
+router.use('*', notFoundRoute);
 
 module.exports = router;

f26e1a42e2106c7cc2548e0126063fc66cdad8ef
BoodyElemary <abdelrahman_elemary99@outlook.com>
2023-06-25 01:16:56 +0300

    starting the notification

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const path = require('path');
 const http = require('http');
-const app = require('./src/app');
+const { app, server, io } = require('./src/app');
+
 const database = require(path.join(__dirname, 'src', 'util', 'database'));
 
 database(() => {
-  http.createServer(app).listen(process.env.PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log('server is runing port ', process.env.PORT);
   });
 });
-
